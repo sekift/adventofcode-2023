c4a2ad2c2cb63836238845a5c03b1e404f6dde8b
Mr.lu <sekift@163.com>
2023-12-14 09:24:56 +0800

    增加day13
    增加day13

--- src/com/sekift/www/Day13.java
+++ src/com/sekift/www/Day13.java
@@ -87,6 +87,86 @@ public class Day13 {
 
     public void star2() {
         List<String> res = InputPuzzle.readFileByLines("./day13");
+
+
+        List<List<String>> list = new ArrayList<>();
+        List<String> listTemp = new ArrayList<>();
+        for (String re : res) {
+            if ("".equals(re)) {
+                list.add(listTemp);
+                listTemp = new ArrayList<>();
+                continue;
+            }
+            listTemp.add(re);
+        }
+
+        // 只判断行的
+        int sum = 0;
+        for (List<String> strings : list) {
+            int r1 = this.reflect2(strings);
+            sum += 100 * r1;
+        }
+        System.out.println(sum);
+    }
+
+    public int reflect2(List<String> list) {
+        int res = 0;
+        for (int i = 0; i < list.size() - 1; i++) {
+            String ju1 = list.get(i);
+            String ju2 = list.get(i + 1);
+            // 判断是否只有一个不同，如果有不同那么循环的就必须相同了
+            boolean flag = false;
+            boolean flag1 = false;
+            if (ju1.equals(ju2)) {
+                flag1 = true;
+            } else {
+                int diff = 0;
+                for (int m = 0; m < ju1.length(); m++) {
+                    if (ju1.charAt(m) != ju2.charAt(m)) {
+                        diff++;
+                    }
+                }
+                if (diff == 1) {
+                    flag1 = true;
+                    flag = true;
+                }
+            }
+
+            if (flag1) {
+                int j = 1, count = 0;
+                while (i - j >= 0 && i + 1 + j <= list.size() - 1) {
+
+                    String ju3 = list.get(i - j);
+                    String ju4 = list.get(i + 1 + j);
+                    // 判断是否只有一个不同
+                    boolean flag2 = false;
+                    if (ju3.equals(ju4)) {
+                        flag2 = true;
+                    } else if (flag) {
+                        int diff = 0;
+                        for (int m = 0; m < ju1.length(); m++) {
+                            if (ju3.charAt(m) != ju4.charAt(m)) {
+                                diff++;
+                            }
+                        }
+                        if (diff == 1) {
+                            flag2 = true;
+                        }
+                    }
+
+
+                    if (flag2) {
+                        count++;
+                    }
+                    j++;
+                }
+                if (i - count == 0 || i + count + 1 == list.size() - 1) {
+                    res = (i + 1);
+                    break;
+                }
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
